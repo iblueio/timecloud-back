3c1c8dc601007eedf69ac563d2255d38b17fb670
高鸣飞 <gaomingfei@ZBMAC-C02VX6A1H.local>
2018-06-06 19:12:57 +0800

    [feature] 添加Back, 整合所有模块

--- test/processor.test.js
+++ test/processor.test.js
@@ -4,6 +4,7 @@ const path = require('path')
 const sleep = require('sleep-promise')
 const assert = require('assert')
 
+
 const logger = {
   info:  console.log,
   error: console.log,
@@ -17,7 +18,7 @@ describe('Processor', () => {
     name: 'normal',
     directory: path.resolve(__dirname, 'test-packages', 'normal'),
     script: 'npm start',
-    schedule: '*/3 * * * * *',
+    schedule: '*/5 * * * * *',
     retries: 0,
     timeout: 10000
   }, logger)
@@ -35,9 +36,10 @@ describe('Processor', () => {
     await new Promise(res => {
       core = new Core({ db: { address: 'mongodb://localhost/iblueio' } }, res)
     })
+    // 将job保存至数据库 这里无需调用core.start()运行定时任务
     let job = await processor.setInterval(core)
     assert.equal(job.attrs.name, 'normal')
+    // 这里会将定时任务从数据库中删除
     await processor.clearInterval()
   })
-
 })
